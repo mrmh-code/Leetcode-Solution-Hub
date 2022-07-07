20f06ed24fc753a3e0a93023fdb2d31e9c5951fb
Mehedi Hasan <59336344+M-Mehedi-Hasan@users.noreply.github.com>
2022-07-08 00:12:58 +0530

    Runtime: 39 ms, faster than 13.33% of C++ and Memory Usage: 5.9 MB, less than 74.27% of C++

---
+++ 9. Palindrome Number/3.Palindorme-Number.cpp
@@ -0,0 +1,19 @@
+ 
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        // string temp = to_string(x);
+        // int i = 0, j = temp.size() - 1;
+        // while (i < j)
+        // {
+        //     if (temp[i++] != temp[j--])
+        //     {
+        //         return 0;
+        //     }
+        // }
+
+        // return 1;
+    }
+};
