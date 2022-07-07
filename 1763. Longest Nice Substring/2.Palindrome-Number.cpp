35b7bf0d81fedeb649a0f0f216fecbc9e20d2493
Mehedi Hasan <59336344+M-Mehedi-Hasan@users.noreply.github.com>
2022-07-08 00:01:45 +0530

    Runtime: 35 ms, faster than 18.68% of C++ and Memory Usage: 5.9 MB, less than 74.27% of C++

--- 1763. Longest Nice Substring/2.Palindrome-Number.cpp
+++ 1763. Longest Nice Substring/2.Palindrome-Number.cpp
@@ -1,11 +1,13 @@
- 
 
-class Solution {
+
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-    //  string rev=to_string(x);
-    //   reverse(rev.begin(), rev.end());
-      
-    //  return to_string(x)==rev;
+    bool isPalindrome(int x)
+    {
+        //  string rev=to_string(x);
+        //   reverse(rev.begin(), rev.end());
+
+        //  return to_string(x)==rev;
     }
 };
